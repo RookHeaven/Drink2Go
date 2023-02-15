27c657073b0e950828403aa79f6cbbc0af1e62e3
Roman Grachev <romagra92@gmail.com>
2023-02-15 21:50:59 +0500

    Add promo and finished footer

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,13 @@
-/* в этот файл добавляет скрипты*/
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
