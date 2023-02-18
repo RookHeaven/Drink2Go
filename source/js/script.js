27d959e4680dd3c6eb24eb5116ed08ca3ab67e8e
Roman Grachev <romagra92@gmail.com>
2023-02-18 23:10:27 +0500

    finished the layout and started the states

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,24 @@
+/* Menu */
+
+const mobileMenu = () => {
+  let navMain = document.querySelector('.main-nav');
+  let navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+};
+
+mobileMenu();
+
 /* Slider */
 
 const swiper = new Swiper('.swiper', {
